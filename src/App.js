cca15dd3c6cdc56d870fb7bcb1397c47a9b74eed
SangchoKim <wjdrms1919@gmail.com>
2020-07-19 18:40:22 +0900

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,48 +1,14 @@
-import React from 'react';
-import UseInput from './utilHooks/useInput/useInput'
-import UseTab from './utilHooks/useTab/useTab'
-import UseTitle from './utilHooks/useTitle/useTitle'
-import UseClick from './utilHooks/useClick/useClick'
-import UseConfirm from './utilHooks/useConfirm/useConfirm'
-import UsePreventLeave from './utilHooks/usePreventLeave/usePreventLeave'
-import UseBeforeLeave from './utilHooks/useBeforeLeave/useBeforeLeave'
-import UseFadeIn from './utilHooks/useFadeIn/useFadeIn'
-import UseNetwork from './utilHooks/useNetwork/useNetwork'
-import UseScroll from './utilHooks/useScroll/useScroll'
-import UseNotification from './utilHooks/useNotification/useNotification'
-import UseFullScreen from './utilHooks/useFullScreen/useFullScreen'
-import UseAxios from './utilHooks/useAxios/useAxios'
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <UseInput/>
-      <br/>
-      <UseTab/>
-      <br/>
-      <UseTitle/>
-      <br/>
-      <UseClick/>
-      <br/>
-      <UseConfirm/>
-      <br/>
-      <UsePreventLeave/>
-      <br/>
-      <UseBeforeLeave/>
-      <br/>
-      <UseFadeIn/>
-      <br/>
-      <UseNetwork/>
-      <br/>
-      <UseScroll/>
-      <br/>
-      <UseNotification/>
-      <br/>
-      <UseFullScreen/>
-      <br/>
-      <UseAxios/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div classname="App">
+        <div classname="App-header">
+          <h2>Welcome to React</h2>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
